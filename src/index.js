92b2cfac7f2b6c7414c7234ac74b33117cf7c9b3
codxmayank <mayankcodex@gmail.com>
2024-12-07 21:23:20 +0530

    update lint rules

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-let a = 10;
 
 dotenv.config(); // Load environment variables
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.info(`Server running on port ${PORT}`);
 });
 
 // TODO: express-validator, validation, api docs, deployment, envs, logger
